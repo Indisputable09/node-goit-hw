e9f8f79d615302599369e4592be29fc66117f239
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-10-19 22:06:46 +0300

    Added scripts

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # node-goit-hw
+
+# Get a list of contacts
+
+node index.js --action list
+
+# Get a contact by ID
+
+node index.js --action get --id 5
+
+# Add a contact
+
+node index.js --action add --name Mango --email mango@gmail.com --phone
+322-22-22
+
+# Remove a contact
+
+node index.js --action remove --id=3
--- contacts.js
+++ contacts.js
@@ -7,7 +7,7 @@ async function listContacts() {
   try {
     const fileData = await fs.readFile(contactsPath);
     const parsedFileData = JSON.parse(fileData);
-    console.log('Contacts ', parsedFileData);
+    console.table(parsedFileData);
   } catch (error) {
     console.error(error.message);
   }
@@ -17,9 +17,11 @@ async function getContactById(contactId) {
   try {
     const fileData = await fs.readFile(contactsPath);
     const parsedFileData = JSON.parse(fileData);
-    const chosenContactData = parsedFileData.find(
-      ({ id }) => Number(id) === contactId
-    );
+    const chosenContactData = parsedFileData.find(({ id }) => id === contactId);
+    if (!chosenContactData) {
+      console.log(`There is no contact with such an ID.`);
+      return;
+    }
     console.log('Chosen contact ', chosenContactData);
   } catch (error) {
     console.error(error.message);
@@ -30,11 +32,16 @@ async function removeContact(contactId) {
   try {
     const fileData = await fs.readFile(contactsPath);
     const parsedFileData = JSON.parse(fileData);
-    const filteredContacts = parsedFileData.filter(
-      ({ id }) => Number(id) !== contactId
-    );
-    fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
-    console.log(`Contact with ${contactId} has been successfully removed.`);
+
+    if (parsedFileData.find(({ id }) => id === contactId)) {
+      const filteredContacts = parsedFileData.filter(
+        ({ id }) => id !== contactId
+      );
+      fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+      console.log(`Contact with ${contactId} has been successfully removed.`);
+      return;
+    }
+    console.log(`There is no contact with ${contactId} ID.`);
   } catch (error) {
     console.error(error.message);
   }
@@ -44,12 +51,31 @@ async function addContact(name, email, phone) {
   try {
     const fileData = await fs.readFile(contactsPath);
     const parsedFileData = JSON.parse(fileData);
+    if (
+      parsedFileData.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      console.log(`${name} is already in contacts`);
+      return;
+    } else if (
+      parsedFileData.find(
+        contact => contact.email.toLowerCase() === email.toLowerCase()
+      )
+    ) {
+      console.log(`${email} is already in contacts`);
+      return;
+    } else if (parsedFileData.find(contact => contact.phone === phone)) {
+      console.log(`${phone} is already in contacts`);
+      return;
+    }
     const generatedContactId =
       Number(parsedFileData[parsedFileData.length - 1].id) + 1;
     const contact = { id: generatedContactId.toString(), name, email, phone };
     const newContactList = [...parsedFileData, contact];
     console.log(`Contact ${contact.name} has been successfully added.`);
-    // fs.writeFile(contactsPath, JSON.stringify(newContactList));
+    fs.writeFile(contactsPath, JSON.stringify(newContactList));
+    return;
   } catch (error) {
     console.error(error.message);
   }
